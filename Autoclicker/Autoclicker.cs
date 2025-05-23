90af9c34107dfaef91f7568999b825d10a1b1cee
Oliver Davis <olliedavis2099@gmail.com>
2025-05-23 17:56:08 +0100

    Creation of project, basics of autoclicker methods, experimenting with P/Invoke to get a jist of functions

---
+++ Autoclicker/Autoclicker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoclicker {
+    internal class Autoclicker {
+
+        private int clicksPerSec;
+
+        public Autoclicker(int initialCPS) {
+            if (initialCPS > 0)
+                clicksPerSec = initialCPS;
+            else clicksPerSec = 0;
+        }
+
+        public void startClicking() {
+
+        }
+
+        public void stopClicking() {
+
+        }
+
+        public bool increaseCPS(int val) {
+            if (clicksPerSec > 0 && val > 0)
+                clicksPerSec += val;
+            else
+                return false;
+            return true;
+        }
+        public bool decreaseCPS(int val) {
+            if (clicksPerSec > 0 && val > 0)
+                clicksPerSec -= val;
+            else
+                return false;
+            return true;
+        }
+    }
+}
---
+++ Autoclicker/PinvokeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoclicker {
+
+    internal class PinvokeTest {
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+        public static void runTest() {
+            // Invoke the function as a regular managed method.
+            MessageBox(IntPtr.Zero, "Somet's gone wrong, Gromit", "'ELP!", 0);
+        }
+
+    }
+}
