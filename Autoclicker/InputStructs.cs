e39d2a26096190d8ab007a8c29b0d294e0d8261e
Oliver Davis <olliedavis2099@gmail.com>
2025-05-25 21:09:37 +0100

    Created first input structs and managed to make mouse start clicking!

---
+++ Autoclicker/InputStructs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoclicker {
+
+    [StructLayout(LayoutKind.Sequential)] // SEQUENTIAL = fields are laid out in the order they are declared
+    public struct MOUSEINPUT {
+        int dx; // x movement
+        int dy; // y movement
+        uint mouseData; // scroll wheel / buttons data
+        uint dwFlags; // specifies type of mouse action
+        uint time; // lets system timestamp the event, normally 0
+        IntPtr dwExtraInfo; // optional app defined ptr that's rarely used
+
+        public void setPosition(int x, int y) {
+            dx = x;
+            dy = y;
+        }
+
+        public void setMouseData(uint data) {
+            mouseData = data;
+        }
+
+        public void setFlags(uint flags) {
+            dwFlags = flags;
+        }
+
+        public void setTime(uint time) {
+            this.time = time;
+        }
+
+        public void setExtraInfo(IntPtr extraInfo) {
+            dwExtraInfo = extraInfo;
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct KEYBDINPUT {
+        ushort wVk;  
+        ushort wScan; 
+        uint dwFlags; 
+        uint time; 
+        IntPtr dwExtraInfo; 
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct INPUT {
+        public uint type; // 0 for mouse input, 1 for keyboard input
+        public INPUT_UNION u; // 
+    }
+
+    [StructLayout(LayoutKind.Explicit)] // EXPLICIT = fields are laid out at specific offsets
+    public struct INPUT_UNION {
+        [FieldOffset(0)] public MOUSEINPUT mi; // mouse input
+        [FieldOffset(0)] public KEYBDINPUT ki; // keyboard input
+    }
+}
--- Autoclicker/PinvokeTest.cs
+++ Autoclicker/PinvokeTest.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Autoclicker {
 
     internal class PinvokeTest {
 
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("user32.dll", SetLastError = true)]
         private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
 
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+
         public static void runTest() {
-            // Invoke the function as a regular managed method.
+
+
+            INPUT inp = new INPUT();
+            inp.type = 0;
+            MOUSEINPUT mi = new MOUSEINPUT();
+            mi.setPosition(0, 0);
+            mi.setFlags(0x0002); // MOUSEEVENTF_LEFTDOWN
+            mi.setMouseData(0);
+            mi.setTime(0);
+            mi.setExtraInfo(IntPtr.Zero);
+
+            inp.u.mi = mi;
+
+            INPUT[] pInputs = { inp };
+
             MessageBox(IntPtr.Zero, "Somet's gone wrong, Gromit", "'ELP!", 0);
+            for(int i = 0; i < 10000; i++) {
+                SendInput(1, pInputs, Marshal.SizeOf(typeof(INPUT)));
+                Console.WriteLine("M Down");
+            }
         }
 
     }
